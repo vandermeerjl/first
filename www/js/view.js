f2f2241219c3d2b4231e7fe1c1cef172644814f2
Jennifer Vandermeer <jennifervandermeer@Jennifer-Vandermeers-MacBook-Pro.local>
2015-03-30 13:55:20 -0400

    thirdCommit

--- www/js/view.js
+++ www/js/view.js
@@ -26,16 +26,13 @@ var app = function(app) {
 		
 		gridManager.add(new zim.Grid(mainContent));
 		
-		// prepare right side
-		p.main.related = new zim.Rectangle(300,600,"red");
-		p.main.addChild(p.main.related);
+	
 		
 		// add parts to layout
 		layoutManager.add(
 			new zim.Layout(p.main, 
 				[{object:p.main.logo, 	marginLeft:10, maxHeight:80, width:20, valign:"top"},
-				{object:p.main.content, marginLeft:5, maxHeight:90, align:"middle", backgroundColor:"lightgreen"},
-				{object:p.main.related, marginLeft:5, maxHeight:80, minWidth:20, align:"right", valign:"top"}],
+				{object:p.main.content, marginLeft:5, maxHeight:90, align:"middle", backgroundColor:"lightgreen"}],
 				10, "black", false, new createjs.Shape(), stage)		
 		);		
 		
@@ -43,8 +40,30 @@ var app = function(app) {
 		p.info.name = "info";
 		p.info.setBounds(0,0,stageW,stageH);
 		
-		var infoBacking = new zim.Rectangle(stageW,stageH,"orange");
+		var infoBacking = new zim.Rectangle(stageW,stageH,"white");
 		p.info.addChild(infoBacking);
+
+		var stage = new createjs.Stage("canvas");
+
+		var html = document.createElement('div');
+		html.id = 'googleMap';
+		html.style.height = '500px';
+		html.style.width = '500px';
+		html.style.position = "absolute";
+		html.style.top = 0;
+		html.style.left = 0;
+		html.style.border = '1px solid red';
+
+		document.body.appendChild(html);
+
+		var gg = new createjs.DOMElement(html);
+		gg.x = 20;
+		gg.y = 20;
+		p.info.addChild(gg);
+		stage.update();
+
+		createjs.Tween.get(gg).to({x:400}, 1000);
+		createjs.Ticker.addEventListener("tick", stage);
 		
 		return p;
 		
